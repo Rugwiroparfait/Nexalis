42d036ddeb4b623a2bac2cead8c69b2e66cad2a5
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-10-20 15:41:09 +0200

    Submit response, Get a form response

--- backend/app/api/responses.py
+++ backend/app/api/responses.py
@@ -1,7 +1,43 @@
 from flask import Blueprint
+from app.models.response import Response
+from app import db
 
-bp = Blueprint('responses', __name__)
+bp = Blueprint('responses', __name__, url_prefix='/responses')
 
-@bp.route('/responses', methods=['GET'])
-def get_responses():
-    return "This is the responses API!"
+@bp.route('/', methods=['POST'])
+def submit_response():
+    """
+    Submit a response to a form
+    ---
+    This endpoint allows users to submit responses for a specific form.
+    Payload: JSON object containing the form ID and the answers.
+    Response: JSON object with the submitted response.
+    """
+    data = request.get_json()
+    form_id = data.get('form_id')
+    answers = data.get('answers')
+
+
+    if not form_id or not answers:
+        return jsonify({"error":"Form ID and answers are required"}), 400
+
+    new_response = Response(form_id = form_id, answers=answers)
+    db.session.add(new_response)
+    db.session.commit()
+
+    return jsonify({"response": new_response.to_dict()}), 201
+
+
+@bp.route('/<int:id>', methods=['GET'])
+def get_response(id):
+    """
+    Get a form response
+    ---
+    This endpoint retrieve a specific response by its ID.
+    Response: JSON object with the response details.
+    """
+    response = Response.query.get_or_404(id)
+
+    return jsonify({"response": response.to dict()})
+
+
