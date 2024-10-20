e5ffdc1c217507e59bc5d4ef289889a7f91f4fd0
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-10-20 15:29:28 +0200

    Create,update and delete question (API)'

--- backend/app/api/questions.py
+++ backend/app/api/questions.py
@@ -1,7 +1,42 @@
 from flask import Blueprint
+from app.models.question import Question
+from app import db
 
-bp = Blueprint('questions', __name__)
+bp = Blueprint('questions', __name__, url_prefix='/questions')
 
-@bp.route('/questions', methods=['GET'])
-def get_questions():
-    return "This is the questions API!"
+@bp.route('/', methods=['POST'])
+def create_question():
+    """
+    Create a new questions
+    ---
+    This endpoint creates a new question for a form.
+    Payload: JSON object containing the form ID, question text, and type.
+    Response: JSON object with the created question.
+    """
+    data = request.get_json()
+    form_id = data.get('form_id')
+    text = data.get('text')
+    question_type = data.get('type')
+
+    if not text or not form_id:
+        return jsonify({"error": "Form ID and question text are required"}), 400
+
+    new_question = Question(form_id=form_id, text=text, type=question_type)
+    db.session.add(new_question)
+    db.session.commit()
+    return jsonify({"question": new_question.to_dict()}), 201
+
+@bp.route('<int:id>', methods=['DELETE'])
+def delete_quetion(id):
+    """
+    Delete a question
+    ---
+    This endpoint deletes a question by its ID.
+    Response: JSON message indicating the deletion status.
+    """
+    question = Question.query.get_or_404(id)
+
+    db.session.delete(question)
+    db.session.commit()
+
+    return jsonify({"message":"Question delete successfully"})
